ecf04df84ac13b541c4df935c900cf50b4dae81d
Ruijie Lin <lrj653224490@gmail.com>
2026-05-25 16:46:12 -0500

    fix: fix project structure
    add web dependency
    add Application class

---
+++ src/main/java/io/github/ruijie_lin_42/storage_management_system_backend/StorageManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.ruijie_lin_42.storage_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageManagementSystemBackendApplication.class, args);
+	}
+
+}
